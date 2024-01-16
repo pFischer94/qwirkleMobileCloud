91fe5c5cad931bfdb54bac417eca2cc24c48fe56
Fischer, Patrick (SE-A/35) <patrick.fischer@volkswagen.de>
2024-01-16 19:40:55 +0100

    changed + to add player, unified player adding

--- qwirkleMobileCloud/src/App.tsx
+++ qwirkleMobileCloud/src/App.tsx
@@ -8,7 +8,6 @@ import { useEffect } from 'react'
 // TODO Bilder
 // TODO kleinere Tastatur / Platzproblem ab 5 SpielerInnen
 // TODO Spracherkennung
-// TODO + speichert Person
 // 2 mal f / finish ohne eingabes
 
 function App() {
